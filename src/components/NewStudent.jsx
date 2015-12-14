1d6ba78f6f5659e913a0c0676c2e289d0e93b684
Eric Allen <eric.justin.allen@gmail.com>
2015-12-14 12:30:26 -0700

    Use props in lieu of local state

--- src/components/NewStudent.jsx
+++ src/components/NewStudent.jsx
@@ -23,7 +23,13 @@ export default class NewStudent extends Component {
     return (
       <div>
         <form className="form" action="/students" method="POST" onSubmit={this.handleSubmit}>
-          <ContactFields handleValueChanged={this.handleValueChanged} showValidations={this.state.submitted}/>
+          <ContactFields
+            handleValueChanged={this.handleValueChanged}
+            showValidations={this.state.submitted}
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}
+            dob={this.state.dob}
+          />
           <div className="row">
             <button type="submit">Submit</button>
           </div>
@@ -35,9 +41,16 @@ export default class NewStudent extends Component {
                      handleValueChanged={this.handleValueChanged}
                      label="Relationship"
                      showValidations={this.state.submitted}
+                     value={this.state.guardianRelationship}
           />
 
-          <ContactFields handleValueChanged={this.handleValueChanged} showValidations={this.state.submitted}/>
+          <ContactFields
+            handleValueChanged={this.handleValueChanged}
+            showValidations={this.state.submitted}
+            firstName={this.state.firstName}
+            lastName={this.state.lastName}
+            dob={this.state.dob}
+          />
           <div className="row">
             <button type="submit">Submit</button>
           </div>
